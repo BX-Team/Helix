fbe968563aa5e3c81ad5c83c194a4ecbff7454e3
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-08-22 10:44:35 +0300

    lowercase name

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,9 @@ publishing {
     }
     publications {
         create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name.lowercase()
+            version = project.version.toString()
             from(components.getByName("java"))
         }
     }
